31c4b432d6cb0a2d8d3ad8463d5bac90814a7dd6
NotZeetaa <rodrigo2005contente@gmail.com>
2021-06-23 23:43:40 +0100

    Bring up to Doze Module
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- common/install.sh
+++ common/install.sh
@@ -4,33 +4,35 @@ mkdir -p $MODPATH/system/bin
 
 ui_print " "
 ui_print "    ×××××××××××××××××××××××××××××××"
-ui_print "       Select Adb Mode:  📳     "
+ui_print "       Select Doze Mode:  💤     "
 ui_print "    ×××××××××××××××××××××××××××××××"
 ui_print " "
 ui_print "  Vol+ = Next; Vol- = Select"
 ui_print " "
-ui_print "  1. Adb Enabler "
-ui_print "  2. Adb Disabler "
+ui_print "  1. Light Doze Mode "
+ui_print "  2. Deep Doze Mode "
+ui_print "  3. Force Doze Mode "
 ui_print " "
 ui_print "  Select: "
-AD=1
+DZ=1
 while true; do
-ui_print "   $AD"
+ui_print "   $DZ"
 	if $VKSEL; then
-		AD=$((AD + 1))
+		DZ=$((DZ + 1))
 	else 
 		break
 	fi
-	if [ $AD -gt 2 ]; then
-		AD=1
+	if [ $DZ -gt 3 ]; then
+		DZ=1
 	fi
 done
 ui_print " "
-ui_print "  Selected: $AD"
+ui_print "  Selected: $DZ"
 #
-case $AD in
- 1 ) FCTEXTAD1="Adb Enabler"; cp -af $TMPDIR/mode/adbE $MODPATH/system/bin;;
- 2 ) FCTEXTAD1="Adb Disabler"; cp -af $TMPDIR/mode/adbD $MODPATH/system/bin;;
+case $DZ in
+ 1 ) FCTEXTAD1="Selected Light Doze Mode"; cp -af $TMPDIR/mode/light $MODPATH/system/bin;;
+ 2 ) FCTEXTAD1="Selected Deep Doze Mode"; cp -af $TMPDIR/mode/deep $MODPATH/system/bin;;
+ 3 ) FCTEXTAD1="Selected Force Doze Mode"; cp -af $TMPDIR/mode/force $MODPATH/system/bin;;
 esac
 ui_print " "
 ui_print "- Mode: $FCTEXTAD1 "
